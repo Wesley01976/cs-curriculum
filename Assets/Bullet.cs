53a6c1419b79b0dcea43e16b205430e73933e9a1
Wesley01976 <Wrustvold0226@student.pps.net>
2024-10-04 13:51:16 -0700

    shoot

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private int _speed = 8;
+    public Vector3 targetPos;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, _speed * Time.deltaTime);
+    }
+}
--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -1,24 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Turret : MonoBehaviour
 
 {
     private int player;
     private int distance;
-    private float cooldown;
+    private float cooldown = 1;
     private int projectile;
-    private float firerate;
+    private float firerate = 2;
+    public GameObject bulletprefab;
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            gameObject clone = Instantiate(b)
-            
+            GameObject clone = Instantiate(bulletprefab, transform.position, quaternion.identity);
+            Bullet script = clone.GetComponent<Bullet>();
+            script.targetPos = other.gameObject.transform.position;
+            cooldown = firerate;
         }
+    }
+
+    private void Update()
+    {
+        cooldown -= Time.deltaTime;
     }
 }
