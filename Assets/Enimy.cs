c002d71b7573ab824fdc581ac2a64ab8eb1f4dc3
Wesley01976 <Wrustvold0226@student.pps.net>
2024-10-10 13:56:16 -0700

    Weeeeeeeee

--- Assets/Enimy.cs
+++ Assets/Enimy.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enimy : MonoBehaviour
@@ -8,11 +10,28 @@ public class Enimy : MonoBehaviour
 {
     public List<Transform> waypoints = new List<Transform>();
 
-    private float _speed;
+    private float _speed = 3f;
     private int currentPoint;
-    private int targetPos;
-    
-    
-    void Start 
 
+
+    private void Start()
+    {
+        currentPoint = 0;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentPoint].position, _speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, waypoints[currentPoint].position)<0.2f)
+        {
+            if (currentPoint<2)
+            {
+                currentPoint += 1;
+            }
+            else
+            {
+                currentPoint = 0;
+            }
+        }
+    }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -12,7 +12,7 @@ public class Health : MonoBehaviour
         gm = FindFirstObjectByType<GameManager>();
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Spikes"))
         {
