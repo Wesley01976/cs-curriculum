078b581c3fce7350de45f56a843ed8c057165da2
Wesley01976 <Wrustvold0226@student.pps.net>
2024-11-18 13:51:20 -0800

    jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -14,6 +15,11 @@ public class Movement : MonoBehaviour
     float Horiziontal;
     public Transform trans;
     float Vertical;
+    public bool overworld;
+    private bool jump;
+    private RaycastHit2D leftray;
+    private RaycastHit2D rightray;
+    
 
 
     // Start is called before the first frame update
@@ -35,7 +41,24 @@ public class Movement : MonoBehaviour
         trans.Translate(new Vector3(xVector, yVector, 0));
         yDirection = Input.GetAxis("Vertical");
         yVector = ySpeed * yDirection * Time.deltaTime;
-       
+      
+        leftray = Physics2D.Raycast(new Vector2(transform.position.x-trans.localScale.x/2,transform.position.y),-Vector2.up, 0.3f);
+        this.rightray = Physics2D.Raycast(new Vector2(transform.position.x+trans.localScale.x/2,transform.position.y),-Vector2.up, 0.3f);
+        if (overworld != true)
+        {
+            jump = Input.GetKeyDown(KeyCode.Space);
+        }
+
+        xVector = Time.deltaTime * xSpeed * xDirection; 
+        yVector = Time.deltaTime * ySpeed * yDirection;
+        trans.Translate(new Vector3(xVector, yVector, 0));
+        leftray = Physics2D.Raycast (new Vector2(transform.position.x - trans. localScale.x / 3, transform.position.y), -Vector2.up, 0.9f);
+        this.rightray = Physics2D.Raycast (new Vector2(transform.position.x + trans. localScale.x/3, transform.position.y), -Vector2.up, 0.9f); 
+        if ((leftray.collider != null ||rightray.collider != null) && jump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 7.5f), ForceMode2D.Impulse);
+        }
+
 
     }
     
