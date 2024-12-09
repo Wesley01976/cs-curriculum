926039c6321ec6c197e48338ff5a8cf0f6aa48fb
Wesley01976 <Wrustvold0226@student.pps.net>
2024-12-09 13:51:50 -0800

    built

---
+++ Assets/Platform_2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform2 : MonoBehaviour
+{
+    private float _speed = 2f;
+    public List<Transform> waypoints = new List<Transform>();
+    private int currentPoint;
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentPoint].position,_speed * Time.deltaTime);
+        
+        if (Vector2.Distance(transform.position, waypoints[currentPoint].position) < 0.2f)
+        {
+            if (currentPoint < 1)
+            {
+                currentPoint += 1;
+            }
+            else
+            {
+                currentPoint = 0;
+            }
+        }
+    }
+}
